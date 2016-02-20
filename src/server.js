b474554c4ee36f7b4b91f95b216c719027440a1c
Anton Matveev <aush.tx@gmail.com>
2016-02-20 07:10:48 +0700

    add favicon

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import 'babel-polyfill';
 import Koa from 'koa';
 import send from 'koa-send';
@@ -7,6 +8,8 @@ app.use(async (ctx, next) => {
   console.log(ctx.path);
   if (ctx.path === '/bundle.js' || ctx.path === '/bundle.css') {
     await send(ctx, ctx.path, { root: __dirname });
+  } else if (ctx.path === '/favicon.ico') {
+    await send(ctx, ctx.path, { root: path.join(__dirname, '..', 'assets') });
   } else {
     await next();
   }
